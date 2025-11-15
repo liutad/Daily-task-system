946d94e2b33a269ba8b9cd8007c5daf2938ee4ba
liutad <138673475+liutad@users.noreply.github.com>
2025-11-15 22:48:51 +0800

    Add files via upload

---
+++ system 1/service-worker.js
@@ -0,0 +1,22 @@
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open("task-app-cache").then(cache => {
+      return cache.addAll([
+        "index.html",
+        "Task.html",
+        "system1.html",
+        "Performances.html",
+        "Strategy.html",
+        "manifest.json",
+        "icons/icon-192.png",
+        "icons/icon-512.png"
+      ]);
+    })
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
+  );
+});
