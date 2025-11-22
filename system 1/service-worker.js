15ef2c46278fad6a23004f943d483dbfd9a86d2e
liutad <138673475+liutad@users.noreply.github.com>
2025-11-22 14:29:27 +0800

    Update service-worker.js

--- system 1/service-worker.js
+++ system 1/service-worker.js
@@ -7,6 +7,7 @@ self.addEventListener("install", e => {
         "system1.html",
         "Performances.html",
         "Strategy.html",
+        "Login.html",
         "manifest.json",
         "icons/icon-192.png",
         "icons/icon-512.png"
@@ -20,3 +21,4 @@ self.addEventListener("fetch", e => {
     caches.match(e.request).then(res => res || fetch(e.request))
   );
 });
+
